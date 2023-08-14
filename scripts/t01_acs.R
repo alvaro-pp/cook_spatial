c69ab581efa3fc76161846a67ee582467236bb7b
theloniousgoerz <theloniouslgoerz@gmail.com>
2023-08-14 12:58:19 -0400

    1. Finished initial draft of acs script. 2. Saved copy of acs data to folder.

--- scripts/t01_acs.R
+++ scripts/t01_acs.R
@@ -10,10 +10,11 @@
 rm(list = ls())
 library(tidycensus)
 library(tidyverse)
+library(here)
 
 # vars19 <- load_variables(year = 2019, "acs5")
 
-get_acs(geography = "tract",
+acs_2019_cook <- get_acs(geography = "tract",
         state = "IL",
         county = "Cook",
         
@@ -42,74 +43,24 @@ get_acs(geography = "tract",
           n_m_blw_poverty_65to74 = "B17001_015",
           n_m_blw_poverty_74up = "B17001_016",
 # Female
-          B17001_017
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:
-            POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          549
-          B17001_018
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!Under 5 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          550
-          B17001_019
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!5 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          551
-          B17001_020
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!6 to 11 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          552
-          B17001_021
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!12 to 14 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          553
-          B17001_022
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!15 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          554
-          B17001_023
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!16 and 17 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          555
-          B17001_024
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!18 to 24 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          556
-          B17001_025
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!25 to 34 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          557
-          B17001_026
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!35 to 44 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          558
-          B17001_027
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!45 to 54 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          559
-          B17001_028
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!55 to 64 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          560
-          B17001_029
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!65 to 74 years
-          POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE
-          tract
-          561
-          B17001_030
-          Estimate!!Total:!!Income in the past 12 months below poverty level:!!Female:!!75 years and over
-          
+          n_f_blw_poverty_level = "B17001_017",
+          n_f_blw_poverty_under5 =  "B17001_018",
+          n_f_blw_poverty_5years = "B17001_019",
+          n_f_blw_poverty_6to11 = "B17001_020",
+          n_f_blw_poverty_12to14 = "B17001_021",
+          n_f_blw_poverty_15years = "B17001_022",
+          n_f_blw_poverty_16to17 = "B17001_023",
+          n_f_blw_poverty_18to24 = "B17001_024",
+n_f_blw_poverty_25to34 = "B17001_025",
+n_f_blw_poverty_35to44 = "B17001_026",
+n_f_blw_poverty_45to54 = "B17001_027",
+n_f_blw_poverty_55to64 = "B17001_028",
+n_f_blw_poverty_65to74 = "B17001_029",
+n_f_blw_poverty_75up = "B17001_030"
         ),
         survey = "acs5",
-        year = 2019)
+        year = 2019,
+output = "wide")
+
+
+write_rds(acs_2019_cook,here("data","ACS_2019", "acs_2019.Rds"))
